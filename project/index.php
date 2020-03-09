ddd8e2c637ceaf6fc89a8a6a7b0805930f577d9b
oathYc <1316796206@qq.com>
2020-03-09 15:54:24 +0800

    code

--- project/index.php
+++ project/index.php
@@ -6,14 +6,14 @@
 <script>
     ws = new WebSocket('ws://127.0.0.1:7865');
     ws.onopen = function(){
-        var his = document.getElementById('history').value;
-        his += '用户进入'+"\n";
-        document.getElementById('history').value = his;
+        // var his = document.getElementById('history').value;
+        // his += '用户进入'+"\n";
+        // document.getElementById('history').value = his;
     };
     ws.onmessage = function(e){
         var data = JSON.parse(e.data);
-        var str = data.userId+'用户：'+data.content;
-        console.log(data);
+        var str = data.userId+'：'+data.content;
+        // console.log(data);
         var his = document.getElementById('history').value;
         his += str+"\n";
         document.getElementById('history').value = his;
@@ -23,8 +23,8 @@
         var data = {'userId':'oathYc','content':sub};
         data = JSON.stringify(data);
         ws.send(data);
-        var his = document.getElementById('history').value;
-        his += '用户发言：'+sub+"\n";
-        document.getElementById('history').value = his;
+        // var his = document.getElementById('history').value;
+        // his += '用户发言：'+sub+"\n";
+        // document.getElementById('history').value = his;
     }
 </script>
--- project/mysql.php
+++ project/mysql.php
@@ -5,23 +5,47 @@ require_once './../Workerman/Mysql/Connection.php';
 
 require_once __DIR__.'/../Workerman/Autoloader.php';
 global $mysql;
+$uid = 0;
 $mysql = new \Workerman\MySQL\Connection('127.0.0.1',3306,'root','root','renma');
 $http_word = new Worker('websocket://0.0.0.0:7865');
 
 $http_word->count=1;
 
 $http_word->onConnect = function($connection){
-    $data = ['userId'=>'系统','content'=>'系统就位'];
-    $data = json_encode($data);
-    $connection->send($data);
+    global $uid,$http_word;
+    $uid += 1;
+    $user = 'user'.$uid;
+    $connection->uid = $user;
+    foreach($http_word->connections as $conn){
+        $data = ['userId'=>$user,'content'=>'我来了'];
+        $data = json_encode($data);
+        $conn->send($data);
+    }
 };
 $http_word->onMessage= function($connection,$data){
+    global $http_word;
+    foreach($http_word->connections as $conn){
+        if(is_string($data)){
+            $data = json_decode($data,true);
+        }
+        $send = ['userId'=>$connection->uid,'content'=>$data['content']];
+        $send = json_encode($send);
+        $conn->send($send);
+    }
 //    global $mysql;
 //    $data = $mysql->select('title')->from('cy_product')->row();
 //    $data = $mysql->select('title')->from('cy_product')->where("id > 34")->query();
 //    $data = $mysql->insert('cy_product')->cols(['title'=>'ceshi','price'=>100])->query();
 //    var_dump($data);
-    $connection->send($data);
+//    $connection->send($data);
+};
+$http_word->onClose = function($connection){
+    global $http_word;
+    foreach($http_word->connections as $conn){
+        $data  = ['userId'=>$connection->uid,'content'=>'我走了，下次见'];
+        $send = json_encode($data);
+        $conn->send($send);
+    }
 };
 Worker::runAll();
 
